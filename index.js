ceff2216cc1ffae980e7aca33422a06cc4cdf9b7
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-09-06 22:24:06 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
-var url = 'mongodb+srv://mooh2jj:doseong9114!!@cluster0.cwn7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+var url =
+  "mongodb+srv://mooh2jj:doseong9114!!@cluster0.cwn7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // mongoose.connect(url,
 //     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 // ).then(()=> console.log('MongoDB Connected...'))
 //     .catch(err => console.log(err))
 mongoose
-.connect(url)
-.then(() => console.log('MongoDB Connected...'))
-.catch((e) => console.log('MongoDB error: ', e));
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((e) => console.log("MongoDB error: ", e));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!!! 안녕하세요!~~~22233");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!! 안녕하세요!~~~222')
-})
+app.post("/register", (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣는다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
