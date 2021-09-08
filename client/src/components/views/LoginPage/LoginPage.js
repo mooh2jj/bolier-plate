306896dcf2e4b937c12e54c5ae2fd1fc7c86f06a
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-09-09 01:29:06 +0900

    로그인, 회원가입, 로그아웃, auth 처리 마무리 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState} from "react";
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+
+function LoginPage(props) {
+
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+      setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+      setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+      event.preventDefault();   // 로그인 버튼만 눌러도 refresh 하는 걸 막아줌
+
+      console.log('Email', Email)
+      console.log('Password', Password)
+
+      let body = {
+          email: Email,
+          password: Password
+      }
+      // dispatch 사용해서 Action 취한다.
+      // landingPage로 이동한다.
+      dispatch(loginUser(body))
+          .then(response => {
+              if (response.payload.loginSuccess) {
+                  props.history.push('/')
+              } else {
+                  alert('Error˝')
+              }
+          })
+    }
+
+    return (
+      <div style={{
+          display: 'flex', justifyContent: 'center', alignItems: 'center'
+          , width: '100%', height: '100vh'
+      }}>
+          <form style={{ display: 'flex', flexDirection: 'column' }}
+              onSubmit={onSubmitHandler}
+          >
+              <label>Email</label>
+              <input type="email" value={Email} onChange={onEmailHandler} />
+              <label>Password</label>
+              <input type="password" value={Password} onChange={onPasswordHandler} />
+              <br />
+              <button type="submit">
+                  Login
+              </button>
+          </form>
+      </div>
+    )
 }
 
-export default LoginPage;
+export default withRouter(LoginPage)
